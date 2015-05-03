25ec276d395ee3143e26f4d0e65280f739ff6615
Jesus <jesus_llorente_gutierrez@hotmail.es>
2015-05-03 02:08:14 +0200

    Asignatura implementada

--- unit8/src/unit8/Asignatura.java
+++ unit8/src/unit8/Asignatura.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package unit8;
 
 /**
- *
+ * Clase que contendrá todos los atributos, constructores y los métodos propios 
+ * de los objetos tipo "Asignatura".
  * @author Jesús Llorente Gutiérrez
  */
 public class Asignatura {
+    
+    //-----ATRIBUTOS-----//
+    
+    private String nombre;
+    private double nota;
+    
+    //-----CONSTRUCTORES-----//
+    
+    /**
+     * Constructor parametrizado de la clase Asignatura.
+     * @param nombre --> Nombre de la asignatura.
+     */
+    public Asignatura(String nombre){
+    
+        this.nombre = nombre;
+    }
+    
+    //-----GETTERS-----//
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+    
+    //-----SETTERS-----//
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    
     
 }
